29a086c6c7f015c5d82be3da04eccbce22ce5473
Jason Turner <ithinkyou50@gmail.com>
2025-05-21 21:45:27 +1000

    rebased to jekyll

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1747827770',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
