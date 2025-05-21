b50b430164f48164f2e9636cb4e1eb255f4fced8
Jason Turner <ithinkyou50@gmail.com>
2025-05-21 23:10:07 +1000

    updated icons

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747831611',resources: [
+    cacheName: 'chirpy-1747832882',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
