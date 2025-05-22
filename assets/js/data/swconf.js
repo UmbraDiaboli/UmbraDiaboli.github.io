5b494833210307d648dd90f67c925809c6c75bf3
Jason Turner <ithinkyou50@gmail.com>
2025-05-22 22:24:17 +1000

    fixed typo

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747916341',resources: [
+    cacheName: 'chirpy-1747916592',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
