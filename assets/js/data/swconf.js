b88677d87d54c57fe16f745caf1dce5733fcd53c
Jason Turner <ithinkyou50@gmail.com>
2025-05-22 22:25:57 +1000

    reworded about

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747916592',resources: [
+    cacheName: 'chirpy-1747916720',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
