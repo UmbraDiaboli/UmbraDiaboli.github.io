ace0f7dd46e2c6c80b7d9cab2623691499af139a
Jason Turner <ithinkyou50@gmail.com>
2025-05-22 22:20:29 +1000

    removed filler posts and added about

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747832882',resources: [
+    cacheName: 'chirpy-1747916341',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
